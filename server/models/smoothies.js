2a1c99e0d96827d837beeef7527152a12d00139d
SLeBron <sophieclebron@gmail.com>
2022-11-05 22:36:41 -0600

    added model

--- server/models/smoothies.js
+++ server/models/smoothies.js
@@ -1,5 +1,24 @@
 const { Schema, model } = require('mongoose');
 
 const smoothiesSchema = new Schema({
-    
-})
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    ingredients: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    recipes: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+});
