f69c6bb3d03f80cfb4f4aae024b97f494a998545
SLeBron <sophieclebron@gmail.com>
2022-11-07 22:13:27 -0700

    added smoothie typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,14 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-
+    type Smoothie {
+        _id: ID
+        title: String
+        imageUrl: String
+        ingredients: String
+        recipes: String
+    }
+    type Query {
+        
+    }
 `
