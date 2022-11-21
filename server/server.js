b053977982158995196e21ecb983b53e0e372a34
SLeBron <sophieclebron@gmail.com>
2022-11-21 16:14:56 -0700

    started middleware in server.js

--- server/server.js
+++ server/server.js
@@ -11,3 +11,16 @@ const server = new ApolloServer({
     typeDefs,
     resolvers,
 });
+
+app.use(express.urlencoded({extended: false }));
+app.use(express.static('public'));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
