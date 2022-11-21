f0652af442dc494361e75b7665f3fe240dda4505
SLeBron <sophieclebron@gmail.com>
2022-11-21 16:06:19 -0700

    required express and apollo

--- server/server.js
+++ server/server.js
@@ -3,3 +3,11 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
